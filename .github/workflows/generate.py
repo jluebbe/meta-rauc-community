099bdd51d8376772eadc538fd4dc3c18d7dd06ff
Jan Luebbe <jlu@pengutronix.de>
2025-01-24 17:32:21 +0100

    .github/workflows: use pengutronix-hosted runner
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- .github/workflows/generate.py
+++ .github/workflows/generate.py
@@ -3,34 +3,43 @@
 from jinja2 import Template
 
 TEMPLATE = """
-name: «« layer »» CI
+name: build «« layer »»
 
 on:
-  # Trigger the workflow on push or pull request,
-  # but only for the master branch
   push:
     branches:
       - master
     paths:
+      - '.github/workflows/«« layer »».yml'
       - '«« layer »»/**'
   pull_request:
     branches:
       - master
     paths:
+      - '.github/workflows/«« layer »».yml'
       - '«« layer »»/**'
+  # allow rebuilding without a push
+  workflow_dispatch: {}
+
 jobs:
   build:
     name: «« layer »» Build
-    runs-on: ubuntu-20.04
+    # run on self-hosted runner for the main repo or if vars.BUILD_RUNS_ON is set
+    runs-on: >-
+      ${{
+        (vars.BUILD_RUNS_ON != ''  && fromJSON(vars.BUILD_RUNS_ON)) ||
+        (github.repository == 'rauc/meta-rauc-community' && fromJSON('["self-hosted", "forrest", "build"]')) ||
+        'ubuntu-20.04'
+      }}
     steps:
       - name: Install required packages
         run: |
-          sudo apt-get install diffstat
+          sudo apt-get -q -y --no-install-recommends install diffstat
       - name: Checkout
-        uses: actions/checkout@v3
+        uses: actions/checkout@v4
       «% for name, url in base_layers.items()|list + extra_layers.items()|list %»
       - name: Clone «« name »»
-        run: git clone -b master «« url »»
+        run: git clone --shared --reference-if-able /srv/shared-git/«« name »».git -b master «« url »»
       «% endfor %»
       - name: Initialize build directory
         run: |
@@ -40,9 +49,17 @@ jobs:
           bitbake-layers add-layer ../«« name »»
           «% endfor %»
           bitbake-layers add-layer ../«« layer »»
-          echo 'INHERIT += "rm_work"' >> conf/local.conf
+          if [ -f ~/.yocto/auto.conf ]; then
+            cp ~/.yocto/auto.conf conf/
+            echo 'SOURCE_MIRROR_URL = "http://10.0.2.2/rauc-community/downloads"' >> conf/auto.conf
+          else
+            echo 'SSTATE_MIRRORS = "file://.* https://github-runner.pengutronix.de/rauc-community/sstate-cache/PATH"' >> conf/auto.conf
+            echo 'BB_SIGNATURE_HANDLER = "OEBasicHash"' >> conf/auto.conf
+            echo 'BB_HASHSERVE = ""' >> conf/auto.conf
+            echo 'OPKGBUILDCMD = "opkg-build -Z gzip -a -1n"' >> conf/auto.conf
+            echo 'INHERIT += "rm_work"' >> conf/auto.conf
+          fi
           echo 'DISTRO_FEATURES:remove = "alsa bluetooth usbgadget usbhost wifi nfs zeroconf pci 3g nfc x11 opengl ptest wayland vulkan"' >> conf/local.conf
-          echo 'SSTATE_MIRRORS = "file://.* http://195.201.147.117/sstate-cache/PATH"' >> conf/local.conf
           «% if machine %»
           echo 'MACHINE = "«« machine »»"' >> conf/local.conf
           «% endif %»
@@ -53,10 +70,32 @@ jobs:
           «% for line in conf %»
           echo '«« line »»' >> conf/local.conf
           «% endfor %»
-      - name: bitbake parsing test
+      - name: Show configuration files
+        run: |
+          cd build/conf
+          rgrep . *.conf
+      - name: Test bitbake parsing
         run: |
           source poky/oe-init-build-env build
           bitbake -p
+      - name: Build rauc, rauc-native
+        run: |
+          source poky/oe-init-build-env build
+          bitbake rauc rauc-native
+      - name: Build core-image-minimal
+        run: |
+          source poky/oe-init-build-env build
+          bitbake core-image-minimal --runall=fetch
+      - name: Cache Data
+        env:
+          CACHE_KEY: ${{ secrets.YOCTO_CACHE_KEY }}
+        if: ${{ env.CACHE_KEY }}
+        run: |
+          mkdir -p ~/.ssh
+          echo "$CACHE_KEY" >> ~/.ssh/id_ed25519
+          chmod 600 ~/.ssh/id_ed25519
+          rsync -rvx --ignore-existing build/downloads rauc-community-cache: || true
+          rsync -rvx --ignore-existing build/sstate-cache rauc-community-cache: || true
 """.lstrip()
 
 template = Template(
@@ -74,7 +113,7 @@ template = Template(
 
 default_context = {
     "base_layers": {
-        "poky": "git://git.yoctoproject.org/poky",
+        "poky": "https://github.com/yoctoproject/poky.git",
         "meta-rauc": "https://github.com/rauc/meta-rauc.git"
     },
     "extra_layers": {
@@ -98,7 +137,7 @@ contexts = [
         **default_context,
         "layer": "meta-rauc-raspberrypi",
         "extra_layers": {
-            "meta-raspberrypi": "git://git.yoctoproject.org/meta-raspberrypi",
+            "meta-raspberrypi": "https://github.com/agherzan/meta-raspberrypi.git",
         },
         "machine": "raspberrypi4",
         "fstypes": "ext4",
